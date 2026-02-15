ade77cbdecd3250c63fe4832cf177266337fe65f
adryan297 <adryanekaputra@gmail.com>
2026-02-15 15:32:36 +0700

    Add dashboard form designer

---
+++ DemoPR.WinForms/FormLogin.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace DemoPR.WinForms
+{
+    partial class FormLogin : Form
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelContainer = new Panel();
+            lblTitle = new Label();
+            lblUsername = new Label();
+            txtUsername = new TextBox();
+            lblPassword = new Label();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            panelContainer.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelContainer
+            // 
+            panelContainer.BackColor = Color.White;
+            panelContainer.BorderStyle = BorderStyle.FixedSingle;
+            panelContainer.Controls.Add(btnLogin);
+            panelContainer.Controls.Add(txtPassword);
+            panelContainer.Controls.Add(lblPassword);
+            panelContainer.Controls.Add(txtUsername);
+            panelContainer.Controls.Add(lblUsername);
+            panelContainer.Controls.Add(lblTitle);
+            panelContainer.Location = new Point(84, 42);
+            panelContainer.Name = "panelContainer";
+            panelContainer.Size = new Size(332, 267);
+            panelContainer.TabIndex = 0;
+            // 
+            // lblTitle
+            // 
+            lblTitle.AutoSize = true;
+            lblTitle.Font = new Font("Segoe UI", 16F, FontStyle.Bold, GraphicsUnit.Point);
+            lblTitle.Location = new Point(104, 28);
+            lblTitle.Name = "lblTitle";
+            lblTitle.Size = new Size(119, 30);
+            lblTitle.TabIndex = 0;
+            lblTitle.Text = "Form Login";
+            // 
+            // lblUsername
+            // 
+            lblUsername.AutoSize = true;
+            lblUsername.Location = new Point(41, 86);
+            lblUsername.Name = "lblUsername";
+            lblUsername.Size = new Size(63, 15);
+            lblUsername.TabIndex = 1;
+            lblUsername.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(41, 104);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(252, 23);
+            txtUsername.TabIndex = 2;
+            // 
+            // lblPassword
+            // 
+            lblPassword.AutoSize = true;
+            lblPassword.Location = new Point(41, 140);
+            lblPassword.Name = "lblPassword";
+            lblPassword.Size = new Size(57, 15);
+            lblPassword.TabIndex = 3;
+            lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(41, 158);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(252, 23);
+            txtPassword.TabIndex = 4;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(0, 123, 255);
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(41, 202);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(252, 35);
+            btnLogin.TabIndex = 5;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // FormLogin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(240, 244, 248);
+            ClientSize = new Size(500, 350);
+            Controls.Add(panelContainer);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            MaximizeBox = false;
+            Name = "FormLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            panelContainer.ResumeLayout(false);
+            panelContainer.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelContainer;
+        private Label lblTitle;
+        private Label lblUsername;
+        private TextBox txtUsername;
+        private Label lblPassword;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
